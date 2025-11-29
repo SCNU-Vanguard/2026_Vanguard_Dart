6e4c15cd6b41e3e8ebeea9e5ff08c73b55206820
bale-deng <3150542185@qq.com>
2025-11-30 02:53:32 +0800

    架构规划

--- 2026_Vanguard_Dart_STM32_Ctrl/Dart_Ctrl/MDK-ARM/User/inc/UserTask.h
+++ 2026_Vanguard_Dart_STM32_Ctrl/Dart_Ctrl/MDK-ARM/User/inc/UserTask.h
@@ -3,5 +3,12 @@
 
 #include "main.h"
 #include "FreeRTOS.h"
+#include "bsp_dwt.h"
+#include "bsp_can.h"
+#include "bsp_uart.h"
+#include "HX06L.h"
+#include "CanMotor.h"
+#include "DM_Motor.h"
+#include "RM_Motor.h"
 
 #endif
